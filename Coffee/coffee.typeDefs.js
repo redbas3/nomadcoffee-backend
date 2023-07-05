4ec6375145a3842d16d6739a446e493b0ddca927
RJTmini <redbas3@gmail.com>
2023-07-05 09:12:01 +0900

    challenge#02_230705

--- Coffee/coffee.typeDefs.js
+++ Coffee/coffee.typeDefs.js
@@ -7,7 +7,29 @@ export default gql`
     createdAt: String!
     updatedAt: String!
   }
+  type User {
+    id: Int!
+    username: String!
+    email: String!
+    name: String!
+    location: String!
+    avatarURL: String!
+    githubUsername: String!
+    createdAt: String!
+    updatedAt: String!
+  }
   type Query {
     coffees: [Coffee]
+  }
+  type Mutation {
+    createAccount(
+      username: String!
+      email: String!
+      name: String!
+      location: String
+      password: String!
+      avatarURL: String!
+      githubUsername: String!
+    ): Boolean
   }
 `;
